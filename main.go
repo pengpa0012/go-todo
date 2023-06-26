d66ad2cebc71401b0e9483ba024a8be7cd9c82c2
Pengpa0012 <peng.ilikha@gmail.com>
2023-06-26 22:40:16 +0800

    added loop

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var inputStr string
+	// reader := bufio.NewReader(os.Stdin)
+	// char, _, _ := reader.ReadRune()
+
 	var todoList []string
+
 	fmt.Println("GO TODO APP")
-	fmt.Scanln(&inputStr)
-	todoList = append(todoList, inputStr)
-	fmt.Println(todoList)
+	for {
+		var inputStr string
+		fmt.Scanln(&inputStr)
+
+		todoList = append(todoList, inputStr)
+		fmt.Println(todoList)
+
+		if inputStr == "x" {
+			fmt.Println("Exiting...")
+			break
+		}
+
+	}
+
 }
